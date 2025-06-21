b3c3ce43b7d0c4dca416aaffc105b083e35e7f8c
arlohb <arlo@urwick.co.uk>
2025-06-21 15:07:07 +0100

    Switched to TFT_eSPI library

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,18 @@
 #include <fmt/core.h>
 #include <Arduino.h>
+#include <TFT_eSPI.h>
 
-#include "lcd/LCD_Driver.h"
+TFT_eSPI display;
 
 void setup() {
     Serial.begin(115200);
+    Serial.println("Setting up display...");
+    display.begin();
 
-    Serial.println("setup: Config_Init");
-    Serial.flush();
-    Config_Init();
-    Serial.println("setup: LCD_Init");
-    Serial.flush();
-    LCD_Init();
-
-    LCD_SetBacklight(255);
-
-    Serial.println(fmt::format("Hello world! 🤖").c_str());
+    pinMode(TFT_BL, OUTPUT);
+    analogWrite(TFT_BL, 255);
+    
+    Serial.println("Setup complete");
 }
 
 bool flip = false;
@@ -24,9 +21,9 @@ void loop() {
     Serial.println("loop");
 
     if (flip) {
-        LCD_Clear(0x0000);
+        display.fillScreen(TFT_RED);
     } else {
-        LCD_Clear(0xF800); // Red in RGB565
+        display.fillScreen(TFT_BLUE);
     }
     flip = !flip;
     
