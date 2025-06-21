e11a9f67c35ca7163e4d2567803122e77109bdc4
arlohb <arlo@urwick.co.uk>
2025-06-21 14:25:05 +0100

    Added display

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <fmt/core.h>
 #include <Arduino.h>
 
+#include "lcd/LCD_Driver.h"
+
 void setup() {
     Serial.begin(115200);
+
+    Serial.println("setup: Config_Init");
+    Serial.flush();
+    Config_Init();
+    Serial.println("setup: LCD_Init");
+    Serial.flush();
+    LCD_Init();
+
+    LCD_SetBacklight(255);
+
     Serial.println(fmt::format("Hello world! 🤖").c_str());
 }
 
-void loop() {}
+bool flip = false;
+
+void loop() {
+    Serial.println("loop");
+
+    if (flip) {
+        LCD_Clear(0x0000);
+    } else {
+        LCD_Clear(0xF800); // Red in RGB565
+    }
+    flip = !flip;
+    
+    delay(1000);
+}
 
